29995f191850fb56a4d134d481e974e7e17631d1
Mustafa Mahmoud <hamzawy2019579@gmail.com>
2023-07-27 00:39:43 +0300

    Data base connection

---
+++ config/connectDB.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const connectDB = async()=> {
+    try {
+        const options = {
+            useUnifiedTopology : true, // to use the new engine of the mongoDB
+            useNewUrlParser : true    // to enable the new pareser for parsing the connection string
+        };
+
+        await mongoose.connect(process.env.CONNECTION_STRING, options);
+        return true;
+    } catch (error) {
+        console.error(error.message);
+        exit(1);
+    }
+}
+
+
+module.exports = connectDB;
