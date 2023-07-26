ad4758c49896b4379220afe7836cedfd214f2fb3
Mustafa Mahmoud <hamzawy2019579@gmail.com>
2023-07-27 00:39:34 +0300

    Update server.js

---
+++ server.js
@@ -0,0 +1,43 @@
+// Importing required modules
+const express = require('express');
+const connectDB = require('./config/connectDB');
+const ErrorHandlerMiddleware = require('./middlewares/ErrorHandlerMiddleware');
+const userRoutes = require('./routes/user');
+const status = require('./config/statusCodes');
+
+// Loading env file
+require('dotenv').config();
+
+const app = express();
+
+// Listening for any request, if connected to db successfully
+const PORT = process.env.PORT || 3005;
+if(connectDB()){
+    console.log('connected to db.');
+
+    // Listening for any request, if connected to db successfully
+    app.listen(PORT, ()=>{
+        console.log(`Server is running on port : ${PORT}`);
+    })
+}
+
+app.use(express.json()) // Built-in Middleware To parse any request body
+
+
+
+/***************************   Routes  *************************************/
+app.use('/user', userRoutes);
+
+
+/***************************   Middlewares  *************************************/
+app.use(ErrorHandlerMiddleware);
+
+
+// Fallback route for any non-existing requested routes.
+app.all('*', (req, res)=>{
+    res.status(status.NOT_FOUND);
+    throw new Error("Not found route.");
+});
+
+
+
