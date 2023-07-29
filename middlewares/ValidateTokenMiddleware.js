c7fad95c9cbee1cdf6fd286825562c737b7f45da
Mustafa Mahmoud <hamzawy2019579@gmail.com>
2023-07-29 22:07:20 +0300

    Updated the middleware that validates the login token

--- middlewares/ValidateTokenMiddleware.js
+++ middlewares/ValidateTokenMiddleware.js
@@ -1,24 +1,23 @@
 const asyncHandler = require('express-async-handler');
 const jwt = require('jsonwebtoken');
-const status = require('../config/statusCodes');
+const status = require('../helpers/statusCodes');
 
 const ValidateTokenMiddleware = asyncHandler( async (req, res, next)=>{
     const authHeader = req.headers.Authorization || req.headers.authorization;
     if(authHeader && authHeader.startsWith('Bearer')){
         const token = authHeader.split(' ')[1];  // select the token
-        jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decodedData)=>{
+        jwt.verify(token, process.env.JWT_ACCESS_TOKEN_KEY, (err, decodedData)=>{
             if(err){
                 res.status(status.UNAUTHORIZED);
-                throw new Error("Not Authorized")
+                throw new Error(JSON.stringify("Not Authorized"))
             }
-            
             req.user = decodedData;
             next();
         })
     }
     else{
         res.status(status.UNAUTHORIZED);
-        throw new Error("This user is not authenticated.");
+        throw new Error(JSON.stringify("This user is not authenticated."));
     }
 
 
