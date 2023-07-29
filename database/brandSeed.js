00e7c46ebcd3ab1c22d285d80bb9fefaf05a674e
Mustafa Mahmoud <hamzawy2019579@gmail.com>
2023-07-29 22:04:19 +0300

    Added seeding file for the brand

---
+++ database/brandSeed.js
@@ -0,0 +1,156 @@
+
+const brands = [
+    {
+      "brandName": "Adidas",
+      "brandSlug": "adidas",
+      "brandImage": "adidas.jpg"
+    },
+    {
+      "brandName": "Nike",
+      "brandSlug": "nike",
+      "brandImage": "nike.jpg"
+    },
+    {
+      "brandName": "Puma",
+      "brandSlug": "puma",
+      "brandImage": "puma.jpg"
+    },
+    {
+      "brandName": "Reebok",
+      "brandSlug": "reebok",
+      "brandImage": "reebok.jpg"
+    },
+    {
+      "brandName": "New Balance",
+      "brandSlug": "new-balance",
+      "brandImage": "new-balance.jpg"
+    },
+    {
+      "brandName": "Under Armour",
+      "brandSlug": "under-armour",
+      "brandImage": "under-armour.jpg"
+    },
+    {
+      "brandName": "Vans",
+      "brandSlug": "vans",
+      "brandImage": "vans.jpg"
+    },
+    {
+      "brandName": "Converse",
+      "brandSlug": "converse",
+      "brandImage": "converse.jpg"
+    },
+    {
+      "brandName": "Adidas Originals",
+      "brandSlug": "adidas-originals",
+      "brandImage": "adidas-originals.jpg"
+    },
+    {
+      "brandName": "Jordan",
+      "brandSlug": "jordan",
+      "brandImage": "jordan.jpg"
+    },
+    {
+      "brandName": "ASICS",
+      "brandSlug": "asics",
+      "brandImage": "asics.jpg"
+    },
+    {
+      "brandName": "Fila",
+      "brandSlug": "fila",
+      "brandImage": "fila.jpg"
+    },
+    {
+      "brandName": "Timberland",
+      "brandSlug": "timberland",
+      "brandImage": "timberland.jpg"
+    },
+    {
+      "brandName": "Brooks",
+      "brandSlug": "brooks",
+      "brandImage": "brooks.jpg"
+    },
+    {
+      "brandName": "Skechers",
+      "brandSlug": "skechers",
+      "brandImage": "skechers.jpg"
+    },
+    {
+      "brandName": "Ecco",
+      "brandSlug": "ecco",
+      "brandImage": "ecco.jpg"
+    },
+    {
+      "brandName": "Saucony",
+      "brandSlug": "saucony",
+      "brandImage": "saucony.jpg"
+    },
+    {
+      "brandName": "Merrell",
+      "brandSlug": "merrell",
+      "brandImage": "merrell.jpg"
+    },
+    {
+      "brandName": "Vibram",
+      "brandSlug": "vibram",
+      "brandImage": "vibram.jpg"
+    },
+    {
+      "brandName": "Salomon",
+      "brandSlug": "salomon",
+      "brandImage": "salomon.jpg"
+    },
+    {
+      "brandName": "K-Swiss",
+      "brandSlug": "k-swiss",
+      "brandImage": "k-swiss.jpg"
+    },
+    {
+      "brandName": "Mizuno",
+      "brandSlug": "mizuno",
+      "brandImage": "mizuno.jpg"
+    },
+    {
+      "brandName": "Hoka One One",
+      "brandSlug": "hoka-one-one",
+      "brandImage": "hoka-one-one.jpg"
+    },
+    {
+      "brandName": "Diadora",
+      "brandSlug": "diadora",
+      "brandImage": "diadora.jpg"
+    },
+    {
+      "brandName": "Vans Off The Wall",
+      "brandSlug": "vans-off-the-wall",
+      "brandImage": "vans-off-the-wall.jpg"
+    },
+    {
+      "brandName": "Onitsuka Tiger",
+      "brandSlug": "onitsuka-tiger",
+      "brandImage": "onitsuka-tiger.jpg"
+    },
+    {
+      "brandName": "Ellesse",
+      "brandSlug": "ellesse",
+      "brandImage": "ellesse.jpg"
+    },
+    {
+      "brandName": "Le Coq Sportif",
+      "brandSlug": "le-coq-sportif",
+      "brandImage": "le-coq-sportif.jpg"
+    },
+    {
+      "brandName": "Kappa",
+      "brandSlug": "kappa",
+      "brandImage": "kappa.jpg"
+    },
+    {
+      "brandName": "Fubu",
+      "brandSlug": "fubu",
+      "brandImage": "fubu.jpg"
+    }
+  ];
+
+module.exports = brands;
+  
