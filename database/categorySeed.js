b23e84e67b2e6bf26030f59a4823ef42a8471d48
Mustafa Mahmoud <hamzawy2019579@gmail.com>
2023-07-29 22:06:49 +0300

    Added seeding file for the category

---
+++ database/categorySeed.js
@@ -0,0 +1,54 @@
+const categories = [
+  {
+    "categoryName": "Electronics",
+    "categorySlug": "electronics",
+    "categoryImage": "electronics.jpg"
+  },
+  {
+    "categoryName": "Clothing",
+    "categorySlug": "clothing",
+    "categoryImage": "clothing.jpg"
+  },
+  {
+    "categoryName": "Shoes",
+    "categorySlug": "shoes",
+    "categoryImage": "shoes.jpg"
+  },
+  {
+    "categoryName": "Home & Kitchen",
+    "categorySlug": "home-kitchen",
+    "categoryImage": "home-kitchen.jpg"
+  },
+  {
+    "categoryName": "Books",
+    "categorySlug": "books",
+    "categoryImage": "books.jpg"
+  },
+  {
+    "categoryName": "Sports & Outdoors",
+    "categorySlug": "sports-outdoors",
+    "categoryImage": "sports-outdoors.jpg"
+  },
+  {
+    "categoryName": "Beauty & Personal Care",
+    "categorySlug": "beauty-personal-care",
+    "categoryImage": "beauty-personal-care.jpg"
+  },
+  {
+    "categoryName": "Toys & Games",
+    "categorySlug": "toys-games",
+    "categoryImage": "toys-games.jpg"
+  },
+  {
+    "categoryName": "Health",
+    "categorySlug": "health",
+    "categoryImage": "health.jpg"
+  },
+  {
+    "categoryName": "Pet Supplies",
+    "categorySlug": "pet-supplies",
+    "categoryImage": "pet-supplies.jpg"
+  }
+]
+
+module.exports = categories;
