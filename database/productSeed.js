2e73a71f13e59029856c2db69397b05c2e7989bc
Mustafa Mahmoud <hamzawy2019579@gmail.com>
2023-07-30 18:23:06 +0300

    Create productSeed.js

---
+++ database/productSeed.js
@@ -0,0 +1,304 @@
+const products = [
+    {
+      "productTitle": "Product 1",
+      "productCode": "PRD001",
+      "productImages": ["image1.jpg", "image2.jpg"],
+      "productDescription": "Description for Product 1",
+      "productPrice": 25.99,
+      "isAvailable": true,
+      "rate": 4.5,
+      "category": "64c55fd491e9eee65323d23a"
+    },
+    {
+      "productTitle": "Product 2",
+      "productCode": "PRD002",
+      "productImages": ["image3.jpg", "image4.jpg"],
+      "productDescription": "Description for Product 2",
+      "productPrice": 34.99,
+      "isAvailable": true,
+      "rate": 3.8,
+      "category": "64c55fd491e9eee65323d23b"
+    },
+    {
+      "productTitle": "Product 3",
+      "productCode": "PRD003",
+      "productImages": ["image5.jpg", "image6.jpg"],
+      "productDescription": "Description for Product 3",
+      "productPrice": 19.99,
+      "isAvailable": false,
+      "rate": 4.2,
+      "category": "64c55fd491e9eee65323d23c"
+    },
+    {
+      "productTitle": "Product 4",
+      "productCode": "PRD004",
+      "productImages": ["image7.jpg", "image8.jpg"],
+      "productDescription": "Description for Product 4",
+      "productPrice": 45.99,
+      "isAvailable": true,
+      "rate": 4.7,
+      "category": "64c55fd491e9eee65323d23d"
+    },
+    {
+      "productTitle": "Product 5",
+      "productCode": "PRD005",
+      "productImages": ["image9.jpg", "image10.jpg"],
+      "productDescription": "Description for Product 5",
+      "productPrice": 12.99,
+      "isAvailable": true,
+      "rate": 3.5,
+      "category": "64c55fd491e9eee65323d23e"
+    },
+    {
+      "productTitle": "Product 6",
+      "productCode": "PRD006",
+      "productImages": ["image11.jpg", "image12.jpg"],
+      "productDescription": "Description for Product 6",
+      "productPrice": 29.99,
+      "isAvailable": true,
+      "rate": 4.0,
+      "category": "64c55fd491e9eee65323d23a"
+    },
+    {
+      "productTitle": "Product 7",
+      "productCode": "PRD007",
+      "productImages": ["image13.jpg", "image14.jpg"],
+      "productDescription": "Description for Product 7",
+      "productPrice": 38.99,
+      "isAvailable": false,
+      "rate": 4.9,
+      "category": "64c55fd491e9eee65323d23b"
+    },
+    {
+      "productTitle": "Product 8",
+      "productCode": "PRD008",
+      "productImages": ["image15.jpg", "image16.jpg"],
+      "productDescription": "Description for Product 8",
+      "productPrice": 17.99,
+      "isAvailable": true,
+      "rate": 3.2,
+      "category": "64c55fd491e9eee65323d23c"
+    },
+    {
+      "productTitle": "Product 9",
+      "productCode": "PRD009",
+      "productImages": ["image17.jpg", "image18.jpg"],
+      "productDescription": "Description for Product 9",
+      "productPrice": 22.99,
+      "isAvailable": true,
+      "rate": 4.8,
+      "category": "64c55fd491e9eee65323d23d"
+    },
+    {
+      "productTitle": "Product 10",
+      "productCode": "PRD010",
+      "productImages": ["image19.jpg", "image20.jpg"],
+      "productDescription": "Description for Product 10",
+      "productPrice": 32.99,
+      "isAvailable": true,
+      "rate": 4.3,
+      "category": "64c55fd491e9eee65323d23e"
+    },
+    {
+        "productTitle": "Product 11",
+        "productCode": "PRD011",
+        "productImages": ["image21.jpg", "image22.jpg"],
+        "productDescription": "Description for Product 11",
+        "productPrice": 27.99,
+        "isAvailable": true,
+        "rate": 4.6,
+        "category": "64c55fd491e9eee65323d240"
+      },
+      {
+        "productTitle": "Product 12",
+        "productCode": "PRD012",
+        "productImages": ["image23.jpg", "image24.jpg"],
+        "productDescription": "Description for Product 12",
+        "productPrice": 39.99,
+        "isAvailable": true,
+        "rate": 3.9,
+        "category": "64c55fd491e9eee65323d241"
+      },
+      {
+        "productTitle": "Product 13",
+        "productCode": "PRD013",
+        "productImages": ["image25.jpg", "image26.jpg"],
+        "productDescription": "Description for Product 13",
+        "productPrice": 15.99,
+        "isAvailable": false,
+        "rate": 4.4,
+        "category": "64c55fd491e9eee65323d243"
+      },
+      {
+        "productTitle": "Product 14",
+        "productCode": "PRD014",
+        "productImages": ["image27.jpg", "image28.jpg"],
+        "productDescription": "Description for Product 14",
+        "productPrice": 48.99,
+        "isAvailable": true,
+        "rate": 4.1,
+        "category": "64c55fd491e9eee65323d242"
+      },
+      {
+        "productTitle": "Product 15",
+        "productCode": "PRD015",
+        "productImages": ["image29.jpg", "image30.jpg"],
+        "productDescription": "Description for Product 15",
+        "productPrice": 14.99,
+        "isAvailable": true,
+        "rate": 3.7,
+        "category": "64c55fd491e9eee65323d240"
+      },
+      {
+        "productTitle": "Product 16",
+        "productCode": "PRD016",
+        "productImages": ["image31.jpg", "image32.jpg"],
+        "productDescription": "Description for Product 16",
+        "productPrice": 36.99,
+        "isAvailable": false,
+        "rate": 4.5,
+        "category": "64c55fd491e9eee65323d241"
+      },
+      {
+        "productTitle": "Product 17",
+        "productCode": "PRD017",
+        "productImages": ["image33.jpg", "image34.jpg"],
+        "productDescription": "Description for Product 17",
+        "productPrice": 20.99,
+        "isAvailable": true,
+        "rate": 4.9,
+        "category": "64c55fd491e9eee65323d243"
+      },
+      {
+        "productTitle": "Product 18",
+        "productCode": "PRD018",
+        "productImages": ["image35.jpg", "image36.jpg"],
+        "productDescription": "Description for Product 18",
+        "productPrice": 29.99,
+        "isAvailable": true,
+        "rate": 3.2,
+        "category": "64c55fd491e9eee65323d242"
+      },
+      {
+        "productTitle": "Product 19",
+        "productCode": "PRD019",
+        "productImages": ["image37.jpg", "image38.jpg"],
+        "productDescription": "Description for Product 19",
+        "productPrice": 22.99,
+        "isAvailable": true,
+        "rate": 4.8,
+        "category": "64c55fd491e9eee65323d240"
+      },
+      {
+        "productTitle": "Product 20",
+        "productCode": "PRD020",
+        "productImages": ["image39.jpg", "image40.jpg"],
+        "productDescription": "Description for Product 20",
+        "productPrice": 32.99,
+        "isAvailable": true,
+        "rate": 4.3,
+        "category": "64c55fd491e9eee65323d241"
+      },
+      {
+        "productTitle": "Product 21",
+        "productCode": "PRD021",
+        "productImages": ["image41.jpg", "image42.jpg"],
+        "productDescription": "Description for Product 21",
+        "productPrice": 23.99,
+        "isAvailable": true,
+        "rate": 4.2,
+        "category": "64c55fd491e9eee65323d240"
+      },
+      {
+        "productTitle": "Product 22",
+        "productCode": "PRD022",
+        "productImages": ["image43.jpg", "image44.jpg"],
+        "productDescription": "Description for Product 22",
+        "productPrice": 17.99,
+        "isAvailable": false,
+        "rate": 4.0,
+        "category": "64c55fd491e9eee65323d241"
+      },
+      {
+        "productTitle": "Product 23",
+        "productCode": "PRD023",
+        "productImages": ["image45.jpg", "image46.jpg"],
+        "productDescription": "Description for Product 23",
+        "productPrice": 21.99,
+        "isAvailable": true,
+        "rate": 3.6,
+        "category": "64c55fd491e9eee65323d243"
+      },
+      {
+        "productTitle": "Product 24",
+        "productCode": "PRD024",
+        "productImages": ["image47.jpg", "image48.jpg"],
+        "productDescription": "Description for Product 24",
+        "productPrice": 19.99,
+        "isAvailable": true,
+        "rate": 4.7,
+        "category": "64c55fd491e9eee65323d242"
+      },
+      {
+        "productTitle": "Product 25",
+        "productCode": "PRD025",
+        "productImages": ["image49.jpg", "image50.jpg"],
+        "productDescription": "Description for Product 25",
+        "productPrice": 28.99,
+        "isAvailable": true,
+        "rate": 3.5,
+        "category": "64c55fd491e9eee65323d240"
+      },
+      {
+        "productTitle": "Product 26",
+        "productCode": "PRD026",
+        "productImages": ["image51.jpg", "image52.jpg"],
+        "productDescription": "Description for Product 26",
+        "productPrice": 31.99,
+        "isAvailable": false,
+        "rate": 4.4,
+        "category": "64c55fd491e9eee65323d241"
+      },
+      {
+        "productTitle": "Product 27",
+        "productCode": "PRD027",
+        "productImages": ["image53.jpg", "image54.jpg"],
+        "productDescription": "Description for Product 27",
+        "productPrice": 34.99,
+        "isAvailable": true,
+        "rate": 4.9,
+        "category": "64c55fd491e9eee65323d243"
+      },
+      {
+        "productTitle": "Product 28",
+        "productCode": "PRD028",
+        "productImages": ["image55.jpg", "image56.jpg"],
+        "productDescription": "Description for Product 28",
+        "productPrice": 25.99,
+        "isAvailable": true,
+        "rate": 3.8,
+        "category": "64c55fd491e9eee65323d242"
+      },
+      {
+        "productTitle": "Product 29",
+        "productCode": "PRD029",
+        "productImages": ["image57.jpg", "image58.jpg"],
+        "productDescription": "Description for Product 29",
+        "productPrice": 27.99,
+        "isAvailable": true,
+        "rate": 4.8,
+        "category": "64c55fd491e9eee65323d240"
+      },
+      {
+        "productTitle": "Product 30",
+        "productCode": "PRD030",
+        "productImages": ["image59.jpg", "image60.jpg"],
+        "productDescription": "Description for Product 30",
+        "productPrice": 19.99,
+        "isAvailable": true,
+        "rate": 3.7,
+        "category": "64c55fd491e9eee65323d241"
+      }
+  ];
+
+  module.exports = products;
