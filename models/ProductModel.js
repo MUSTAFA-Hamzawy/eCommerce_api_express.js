bb88689054f367d574f8d4ac62138c1ee0432efd
Mustafa Mahmoud <hamzawy2019579@gmail.com>
2023-07-30 18:24:49 +0300

    Created schema for product

---
+++ models/ProductModel.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+    
+        productTitle: {
+            type: String,
+            required: [true, 'Product title is required.']
+        },
+        productCode: {
+            type: String,
+        },
+        productImages: {
+            type: Array,
+            required: [true, 'Product image is required.']
+        },
+        productDescription: {
+            type: String,
+            required: [true, 'Produce description is required.']
+        },
+        productPrice: {
+            type: Number,
+            require: [true, 'Product price is required.']
+        },
+        isAvailable:{
+            type: Boolean,
+            default: true
+        },
+        rate:{
+            type: Number
+        },
+        category:{
+            type: mongoose.Schema.Types.ObjectId,
+            required: [true, 'Product category is required'],
+            ref: 'category'
+        }
+    
+
+},{
+    timestamps: true,
+});
+
+module.exports = mongoose.model('product', ProductSchema);
