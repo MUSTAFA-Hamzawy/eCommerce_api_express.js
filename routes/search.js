e1d1497961c1361236e0d334a134fc9168f71b39
Mustafa Mahmoud <hamzawy2019579@gmail.com>
2023-07-31 20:21:40 +0300

    Create search.js

---
+++ routes/search.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const SearchController = require('../controllers/SearchController');
+
+module.exports = {
+    findBrands,
+    findCategories,
+    findProductsByFilters
+} = SearchController;
+
+
+
+router.get('/product', findProductsByFilters);
+router.get('/brand', findBrands);
+router.get('/category', findCategories);
+
+
+module.exports = router;
