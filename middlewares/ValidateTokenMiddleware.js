1a703a94e37ef82375789e0987ca68637df50576
Mustafa Mahmoud <hamzawy2019579@gmail.com>
2023-08-04 19:16:57 +0300

    Checked if the token is in black list or not

--- middlewares/ValidateTokenMiddleware.js
+++ middlewares/ValidateTokenMiddleware.js
@@ -1,11 +1,21 @@
 const asyncHandler = require('express-async-handler');
 const jwt = require('jsonwebtoken');
 const status = require('../helpers/statusCodes');
+const TokenBlackListModel = require('../models/TokenBlackListModel');
 
 const ValidateTokenMiddleware = asyncHandler( async (req, res, next)=>{
     const authHeader = req.headers.Authorization || req.headers.authorization;
     if(authHeader && authHeader.startsWith('Bearer')){
         const token = authHeader.split(' ')[1];  // select the token
+
+        // Ensuring that this token is not included in the TokenBlackList
+        const found = await TokenBlackListModel.findOne({token});
+        if(found){
+            res.status(status.UNAUTHORIZED);
+            throw new Error(JSON.stringify("This user is not authenticated."));
+        }
+
+        // verifying the token
         jwt.verify(token, process.env.JWT_ACCESS_TOKEN_KEY, (err, decodedData)=>{
             if(err){
                 res.status(status.UNAUTHORIZED);
